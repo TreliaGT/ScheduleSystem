833d7725d8749e4fd88c63a014dd4b23edfa6255
GT <v244682@tafe.wa.edu.au>
2020-01-07 11:46:25 +0800

    removed one issue

--- app/Employee.php
+++ app/Employee.php
@@ -6,9 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    protected $guarded = ['id'];
-
-    protected $hidden = ['id'];
+    
 
     public function schedule()
     {
